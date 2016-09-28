5510f130d9a03835488723ec2d6c2564b4044d7f
nykaj <ajimini2@gmail.com>
2016-09-28 19:53:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ app.get('/', function (req, res) {
 app.get('/Article one', function(req,res){
     res.send('Requested')
 });
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+app.get('/Article two', function (req, res) {
+  res.send('Requested')
 });
 
 app.get('/ui/madi.png', function (req, res) {
